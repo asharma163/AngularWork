a50e98b0542c40309a4afcd11bf4d80616de2169
Anshul Sharma <asharma163@myseneca.ca>
2018-08-08 00:02:44 -0400

    Changes made according to Asiignemnt1 Added functionality for buttons

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-index.html is served by the browser.
-
-<app-root> component is replaced by its content (html).
-
-How is angular triggred to load content of the custome selector i.e. in this case <app-root>?
-
-In the final index.html file, whenever project rebuilt js object bundles added by automation.
-All components except root component are in the app.component.html file.
-
-Use ng g c "component name" to create component from cli.
-
-custom selectors can be used as css selectors also.
+1.  CREATE TWO NEW COMPONENTS - WARNINGALERT AND SUCCESSALERT
+2.  OUTPUT THEM BENEATH EACH OTHER IN THE APPCOMPONENT
+3.  OUTPUT SUCCESS OR WARNING MESSAGES
+4.  STYLE THE COMPONENTS ACCORDINGLY.
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { AppComponent } from "./app.component";
+import { SuccessComponent } from "./success/success.component";
+import { WarningComponent } from "./warning/warning.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SuccessComponent, WarningComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
