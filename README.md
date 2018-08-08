5dc255457cc7fcacc9cb696db9315c2008e7eb8c
Anshul Sharma <asharma163@myseneca.ca>
2018-08-08 14:25:18 -0400

    event binding- in particular click event

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-Databinding is communication between business logic and UI.
-
-String Interpolation -> synatx {{something}}
-Property Binding [property] = "data"
-Event Binding (event) = "expression"
-Two way databinding [(ngModel)] = "something"
+Handling events ->
+Output value of the property.
--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -9,11 +9,16 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = "offline";
   isDisabled = false;
+  serverCreationStatus = "There is no server!";
 
   constructor() {
     setTimeout(() => {
       this.isDisabled = true;
     }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server created!";
   }
 
   ngOnInit() {}
