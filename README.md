f2799c4c33877302fac7a47d389d358c40b5d796
Anshul Sharma <asharma163@myseneca.ca>
2018-08-09 01:28:38 -0400

    Assignment - 2 done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-$event provides with data when imput or click event is fired and this information can be used.
-
-Two way data-binding -> [(ngModel)]
+Add input field which updates property('username') via two-way databinding.
+Output the username property via String Interpolation ( in paragraph below the input)
+Add button which may be clicked if username is NOT empty string
+Upon clicking the button, the username should reset to empty string.
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, InputComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
