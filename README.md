489a79f981b2730d36b882cfe83b3ed9d5ef9277
anshul sharma <anshulsharma427@gmail.com>
2018-08-21 17:15:23 -0400

    Iteration - 1 project

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 (1) Add a button which says 'Display Details'
 (2) Add a paragraph with any content of your choice.(e.g. 'Secret Password = tuna')
 (3) Toggle the displaying of that paragraph with the button created in the first step.
-(4) Log all button clicks in an array and output that array below the secret paragraph. (Log a timestamp/ or incrementing number)
 (5) From 5th log item give all of them blue color.
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,12 +4,25 @@ import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from "./servers/servers.component";
-import { As3Component } from './as3/as3.component';
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, As3Component],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
